dc4f9a00b6cb6f7176664c481d80ee8931ef9fed
lutfi <Abdiladifart1996@gmail.com>
2024-03-19 02:13:21 +0300

    Initial commit

---
+++ src/main/java/com/example/healhub/HealHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.healhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealHubApplication.class, args);
+    }
+
+}
