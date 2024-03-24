19930bccf2803c10fb16338fae37e7977f2b42ef
Abdiladif <abdiladifart1996@gmail.com>
2024-03-24 22:08:43 +0300

    initial

---
+++ src/test/java/com/example/healhub/HealHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.healhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
